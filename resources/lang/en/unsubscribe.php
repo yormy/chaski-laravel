53921077f352dfc1b477194dba6e27dbc1bcc275
Mexion <dev@mexion.com>
2023-07-14 10:35:59 +0200

    unsubscribe result pages

--- resources/lang/en/unsubscribe.php
+++ resources/lang/en/unsubscribe.php
@@ -2,4 +2,20 @@
 
 return [
     'line_1' => 'To unsubscribe to these types of messages <a href=":unsubscribeLink">unsubscribe</a>',
+
+    'success' => [
+        'title' => 'Success',
+        'description' => 'You are successfully unsubscribed',
+    ],
+
+    'unsubscribe_prevented' => [
+        'title' => 'Cannot unsubscribe',
+        'description_line1' => 'You cannot unsubscribe to these types of messages.',
+        'description_line2' => 'This is due to legal or security requirements',
+    ],
+
+    'invalid_token' => [
+        'title' => 'Invalid Token',
+        'description' => 'Something went wrong, we cannot process your request',
+    ],
 ];
