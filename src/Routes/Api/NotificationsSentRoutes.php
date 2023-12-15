56172ef90a4ec1ed7c62389c49d17ac14dccdb3d
Mexion <dev@mexion.com>
2023-12-15 14:26:32 +0100

    Allow excluding polling route from rate limter

--- src/Routes/Api/NotificationsSentRoutes.php
+++ src/Routes/Api/NotificationsSentRoutes.php
@@ -19,11 +19,27 @@ class NotificationsSentRoutes
                         ->name('notifications.')
                         ->group(function () {
                             Route::get('/', [NotificationsSentController::class, 'index'])->name('index');
-                            Route::get('/attention', [NotificationsSentController::class, 'attention'])->name('attention'); // user
                             Route::put('/mark-opened/{id}', [NotificationsSentController::class, 'markOpened'])->name('mark-opened');
                         });
 
                 });
         });
+
+        /**
+         * These routes will be polled every x time, so they need to not be rate-limited
+         */
+        Route::macro('ChaskiNotificationsSentApiPollingRoutes', function (string $prefix = '') {
+            Route::prefix($prefix)
+                ->name('chaski.')
+                ->group(function () {
+                    Route::prefix('notifications')
+                        ->name('notifications.')
+                        ->group(function () {
+                            Route::get('/attention', [NotificationsSentController::class, 'attention'])->name('attention');
+                        });
+
+                });
+        });
+
     }
 }
