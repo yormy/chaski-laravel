f86baa571c9c3bad8b6f6d8ace3c68a7a449ab30
Mexion <dev@mexion.com>
2023-12-28 12:49:08 +0100

    cascade on delete

--- database/migrations/2020_03_01_194010_create_sent_emails_log.php
+++ database/migrations/2020_03_01_194010_create_sent_emails_log.php
@@ -20,7 +20,7 @@ return new class extends Migration
 
             $table->timestamps();
 
-            $table->foreign('sent_email_id')->references('id')->on('sent_emails');
+            $table->foreign('sent_email_id')->references('id')->on('sent_emails')->cascadeOnDelete();
         });
     }
 
