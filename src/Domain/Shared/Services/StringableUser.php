1e4acdb9d82db1ac5e5bda33d2235cdc641c90bd
Rob <rob@email.com>
2024-04-04 17:07:17 +0200

    linters

--- src/Domain/Shared/Services/StringableUser.php
+++ src/Domain/Shared/Services/StringableUser.php
@@ -29,7 +29,7 @@ class StringableUser
 
     public static function fromString(?string $value): ?\stdClass
     {
-        if (!$value) {
+        if (! $value) {
             return null;
         }
 
